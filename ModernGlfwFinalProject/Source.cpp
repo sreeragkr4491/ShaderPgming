7b9b094a886a11042d7fe1c3cd3e8e211c6aa9d2
SreeragKR <80149744+sreeragkr4491@users.noreply.github.com>
2021-07-27 11:07:30 +0530

    Shader code with multi color cube

--- ModernGlfwFinalProject/Source.cpp
+++ ModernGlfwFinalProject/Source.cpp
@@ -633,30 +633,290 @@
 
 //lab 6 cube using shader
 
+//
+//#include<GL/glew.h> //1.must add this library
+//#include<GLFW/glfw3.h>
+//#include<iostream>
+//
+//using namespace std;
+//
+////vertex shader coding part
+//const GLchar* vshader = "#version 330 core \n"
+//"layout (location=0) in vec3 position; \n"     //3.must write the semicolon
+//"void main() \n"								//4.must add () in main
+//"{ \n"
+//"gl_Position = vec4(position,1.0); \n"
+//"} \n"
+//;
+//
+////fragment shader coding part
+//const GLchar* fshader = "#version 330 core \n"
+//"out vec4 color; \n"						//5.must write the semicolon
+//"void main() \n"							//6.must add () in main
+//"{ \n"
+//"color = vec4(1,0,0,1); \n"
+//"} \n"
+//;
+//
+//void main()
+//{
+//	GLint wid = 1200; //*window variable
+//	GLint height = 800; //*window variable
+//	GLFWwindow* window; //pointer var as window that will hold address only 
+//	glfwInit();
+//
+//	if (!glfwInit())
+//	{
+//		cout << "glfw library error" << endl;
+//	}
+//	else
+//	{
+//		cout << "Success og glfw" << endl;
+//	}
+//	window = glfwCreateWindow(wid, height, "Window with background color", NULL, NULL); //*assigned window variable 
+//
+//	// make the window context current
+//	glfwMakeContextCurrent(window);
+//
+//	// 2.   **************** here must add glew function use*********
+//	if (glewInit() != GLEW_OK)
+//	{
+//		cout << "fail to open glew\n";
+//
+//	}
+//	else
+//		cout << "glew works success\n";
+//	// till here glew stuff
+//
+//
+//	GLint success;
+//	GLchar information[512];
+//	//vertex shader 
+//	GLuint verShader = glCreateShader(GL_VERTEX_SHADER);
+//	glShaderSource(verShader, 1, &vshader, NULL);
+//	glCompileShader(verShader);
+//	glGetShaderiv(verShader, GL_COMPILE_STATUS, &success);
+//	if (!success)
+//	{
+//		glGetShaderInfoLog(verShader, 512, NULL, information);
+//		cout << "Error in vertex shader compilation is== " << information << endl;
+//	}
+//	else
+//		cout << "Vertex shader compilation success" << endl;
+//
+//
+//	//fragment shader
+//	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+//	glShaderSource(fragShader, 1, &fshader, NULL);
+//	glCompileShader(fragShader);
+//	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+//	if (!success)
+//	{
+//		glGetShaderInfoLog(fragShader, 512, NULL, information);
+//		cout << "Error in fragment shader compilation is== " << information << endl;
+//	}
+//	else
+//		cout << "Fragment shader compilation success" << endl;
+//
+//
+//
+//
+//
+//	//Shader linking of vertex and fragment shader
+//	GLuint ShaderLink = glCreateProgram();
+//	glAttachShader(ShaderLink, verShader);
+//	glAttachShader(ShaderLink, fragShader);
+//	glLinkProgram(ShaderLink);
+//	glGetShaderiv(ShaderLink, GL_COMPILE_STATUS, &success);
+//	if (!success)
+//	{
+//		glGetShaderInfoLog(ShaderLink, 512, NULL, information);
+//		cout << "Error in linking the shader compilation is== " << information << endl;
+//	}
+//	else
+//		cout << "Shader Linking compilation success" << endl;
+//
+//	glDeleteShader(verShader);
+//	glDeleteShader(fragShader);
+//
+//	GLfloat vertices[] =
+//	{
+//		-0.5f, -0.5f, 0.0f,
+//		0.0f, -0.5f, 0.0f,
+//		0.0f, 0.5f, 0.0f,
+//		-0.5f, 0.5f, 0.0f
+//	};
+//
+//	GLuint VBO, VAO;//vertex buffer obj//vert array obj
+//	glGenVertexArrays(1, &VAO);
+//	glGenBuffers(1, &VBO);
+//
+//	glBindVertexArray(VAO);
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+//	glEnableVertexAttribArray(0);
+//	glBindBuffer(GL_ARRAY_BUFFER, 0);
+//	glBindVertexArray(0);
+//
+//
+//
+//
+//	//gameloop
+//	while (!glfwWindowShouldClose(window))
+//	{
+//		//for the bg color
+//		glClearColor(0.5, 1, 1, 0); //for rgb color change
+//		glClear(GL_COLOR_BUFFER_BIT);//to clear the buffer
+//
+//		//linking the shader / calling the shader
+//		glUseProgram(ShaderLink);
+//		glBindVertexArray(VAO);				//7.add parameter as VAO then it will come to know with buffer we are drawing
+//
+//		//drawing
+//		glDrawArrays(GL_QUADS, 0, 4);
+//		//glBindVertexArray();
+//
+//		glfwSwapBuffers(window);//to swap the new color for window
+//		glfwPollEvents();
+//	}
+//	glDeleteVertexArrays(1, &VAO);
+//	glDeleteBuffers(1, &VBO);
+//	glfwTerminate();
+//}
+//
+//
 
-#include<GL/glew.h> //1.must add this library
+//lab 7 split the shader files
+
+//
+//#include<GL/glew.h> 
+//#include<GLFW/glfw3.h>
+//#include<iostream>
+//#include"Shader.h"     //1.Attach the shader file
+//
+//using namespace std;
+//
+////vertex shader coding part
+//
+//
+////fragment shader coding part
+//
+//
+//void main()
+//{
+//	GLint wid = 1200; //*window variable
+//	GLint height = 800; //*window variable
+//	GLFWwindow* window; //pointer var as window that will hold address only 
+//	glfwInit();
+//
+//	if (!glfwInit())
+//	{
+//		cout << "glfw library error" << endl;
+//	}
+//	else
+//	{
+//		cout << "Success og glfw" << endl;
+//	}
+//	window = glfwCreateWindow(wid, height, "Window with background color", NULL, NULL); //*assigned window variable 
+//
+//	// make the window context current
+//	glfwMakeContextCurrent(window);
+//
+//	// 2.   **************** here must add glew function use*********
+//	if (glewInit() != GLEW_OK)
+//	{
+//		cout << "fail to open glew\n";
+//
+//	}
+//	else
+//		cout << "glew works success\n";
+//	// till here glew stuff
+//
+//
+//	GLint success;
+//	GLchar information[512];
+//	//vertex shader 
+//	
+//	//2.Code have been shifted to vertex file
+//
+//	//fragment shader
+//	
+//	//3.Code have been shifted to frag file
+//
+//
+//
+//	//Shader linking of vertex and fragment shader
+//	
+//
+//	//attach shader files
+//	Shader Myshader("Test1.vs", "Test1.frag");     //4.Create the reference of the class
+//
+//	GLfloat vertices[] =
+//	{
+//		-0.5f, -0.5f, 0.0f,
+//		0.0f, -0.5f, 0.0f,
+//		0.0f, 0.5f, 0.0f,
+//		-0.5f, 0.5f, 0.0f
+//	};
+//
+//	GLuint VBO, VAO;//vertex buffer obj//vert array obj
+//	glGenVertexArrays(1, &VAO);
+//	glGenBuffers(1, &VBO);
+//
+//	glBindVertexArray(VAO);
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+//	glEnableVertexAttribArray(0);
+//	glBindBuffer(GL_ARRAY_BUFFER, 0);
+//	glBindVertexArray(0);
+//
+//
+//
+//
+//	//gameloop
+//	while (!glfwWindowShouldClose(window))
+//	{
+//		//for the bg color
+//		glClearColor(0.5, 1, 1, 0); //for rgb color change
+//		glClear(GL_COLOR_BUFFER_BIT);//to clear the buffer
+//
+//		//linking the shader / calling the shader
+//		Myshader.Use();           //5.use the shader file
+//		glBindVertexArray(VAO);				
+//
+//		//drawing
+//		glDrawArrays(GL_QUADS, 0, 4);
+//		//glBindVertexArray();
+//
+//		glfwSwapBuffers(window);//to swap the new color for window
+//		glfwPollEvents();
+//	}
+//	glDeleteVertexArrays(1, &VAO);
+//	glDeleteBuffers(1, &VBO);
+//	glfwTerminate();
+//}
+//
+//
+//
+
+
+//lab 8 how to apply colors with shader
+
+
+#include<GL/glew.h> 
 #include<GLFW/glfw3.h>
 #include<iostream>
+#include"Shader.h"     //1.Attach the shader file
 
 using namespace std;
 
 //vertex shader coding part
-const GLchar* vshader = "#version 330 core \n"
-"layout (location=0) in vec3 position; \n"     //3.must write the semicolon
-"void main() \n"								//4.must add () in main
-"{ \n"
-"gl_Position = vec4(position,1.0); \n"
-"} \n"
-;
+
 
 //fragment shader coding part
-const GLchar* fshader = "#version 330 core \n"
-"out vec4 color; \n"						//5.must write the semicolon
-"void main() \n"							//6.must add () in main
-"{ \n"
-"color = vec4(1,0,0,1); \n"
-"} \n"
-;
+
 
 void main()
 {
@@ -692,59 +952,27 @@ void main()
 	GLint success;
 	GLchar information[512];
 	//vertex shader 
-	GLuint verShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(verShader, 1, &vshader, NULL);
-	glCompileShader(verShader);
-	glGetShaderiv(verShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(verShader, 512, NULL, information);
-		cout << "Error in vertex shader compilation is== " << information << endl;
-	}
-	else
-		cout << "Vertex shader compilation success" << endl;
 
+	//2.Code have been shifted to vertex file
 
 	//fragment shader
-	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragShader, 1, &fshader, NULL);
-	glCompileShader(fragShader);
-	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragShader, 512, NULL, information);
-		cout << "Error in fragment shader compilation is== " << information << endl;
-	}
-	else
-		cout << "Fragment shader compilation success" << endl;
 
-
+	//3.Code have been shifted to frag file
 
 
 
 	//Shader linking of vertex and fragment shader
-	GLuint ShaderLink = glCreateProgram();
-	glAttachShader(ShaderLink, verShader);
-	glAttachShader(ShaderLink, fragShader);
-	glLinkProgram(ShaderLink);
-	glGetShaderiv(ShaderLink, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(ShaderLink, 512, NULL, information);
-		cout << "Error in linking the shader compilation is== " << information << endl;
-	}
-	else
-		cout << "Shader Linking compilation success" << endl;
 
-	glDeleteShader(verShader);
-	glDeleteShader(fragShader);
+
+	//attach shader files
+	Shader Myshader("Test2.vs", "Test2.frag");     //4.Create the reference of the class
 
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		0.0f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,1,0,0,
+		0.0f, -0.5f, 0.0f,0,1,0,
+		0.0f, 0.5f, 0.0f,0,0,1,
+		-0.5f, 0.5f, 0.0f,1,1,0
 	};
 
 	GLuint VBO, VAO;//vertex buffer obj//vert array obj
@@ -754,8 +982,15 @@ void main()
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	
+	//vertex
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
+	//color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
@@ -770,8 +1005,8 @@ void main()
 		glClear(GL_COLOR_BUFFER_BIT);//to clear the buffer
 
 		//linking the shader / calling the shader
-		glUseProgram(ShaderLink);
-		glBindVertexArray(VAO);				//7.add parameter as VAO then it will come to know with buffer we are drawing
+		Myshader.Use();           //5.use the shader file
+		glBindVertexArray(VAO);
 
 		//drawing
 		glDrawArrays(GL_QUADS, 0, 4);
@@ -784,5 +1019,7 @@ void main()
 	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 }
+
+
 
 
